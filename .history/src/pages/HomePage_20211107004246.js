e5a0446e0d19575609d9916121d774d0a4b883eb
stepanenkoales <stepanenkoales@gmail.com>
2021-11-08 18:38:40 +0300

    1st draft

--- .history/src/pages/HomePage_20211107004246.js
+++
@@ -1,6 +0,0 @@
-import { Navigate } from 'react-router-dom';
-
-export const HomePage = () => {
-    return <Navigate to='/login' />;
-    
-}
--- .history/src/store/store_20211108170113.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit';
-import userReducer from './userCreateSlice';
-
-export const store = configureStore({
-    reducer: {
-        user: userReducer,
-    }
-});
